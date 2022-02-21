7b2f4136566deb632b18e2702d41f43ed959fb44
Jaime-prog <A00571842@tec.mx>
2022-02-21 00:52:05 -0600

    commit

--- Labs Personales/code.js
+++ Labs Personales/code.js
@@ -1,1 +1,15 @@
-document.getElementById("h1").innerHTML="Datos de Lab"
+document.getElementById("h1").innerHTML = "Datos de Lab"
+
+function showImage() {
+    // Get the checkbox
+    var checkBox = document.getElementById("valorant");
+    // Get the output text
+    var text = document.getElementById("text");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
