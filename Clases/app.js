7640f2063170cba20bebc77e43b8b201bd16419f
Jaime-prog <A00571842@tec.mx>
2022-03-02 17:22:43 -0600

    commit

--- Clases/app.js
+++ Clases/app.js
@@ -1,7 +1,24 @@
-fs; fileSystem
+/*fs; fileSystem
 const FileSystem = require('fs');
 fileSystem.writeFileSync('hola.txt','hola desde 0')
 
 
 const http = require('http');
 const server= http.createServer()
+*/
+
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
